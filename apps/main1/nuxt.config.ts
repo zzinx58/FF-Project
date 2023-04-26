2f1c2d46962075049034b7bf3148c8c0468bf0d4
zzinx58 <q1095568627@163.com>
2023-04-27 02:53:00 +0800

    Setup: project eslint config setup and some chore

--- apps/main1/nuxt.config.ts
+++ apps/main1/nuxt.config.ts
@@ -1,24 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@nuxt/devtools",
-    "@nuxtjs/eslint-module",
-    "@nuxt/image-edge",
-    "@nuxt/content",
-    "@nuxtjs/i18n",
-    "@vite-pwa/nuxt",
-    "@nuxtjs/strapi",
-    "@unocss/nuxt",
+    '@nuxt/devtools',
+    '@nuxt/image-edge',
+    '@nuxt/content',
+    '@nuxtjs/i18n',
+    '@vite-pwa/nuxt',
+    '@nuxtjs/strapi',
+    '@unocss/nuxt',
   ],
+  imports: {
+    autoImport: true,
+  },
   strapi: {
     devtools: true,
-    url: "http://localhost:1337",
+    url: 'http://localhost:1337',
   },
-  unocss: {},
+  unocss: {
+    uno: true,
+    icons: true,
+    attributify: true,
+    // core options
+    shortcuts: [],
+    rules: [],
+    safelist: [],
+  },
   pwa: {},
-  eslint: {},
   image: {},
   i18n: {},
   content: {},
-  devtools: {},
-});
+  // devtools: {},
+})
