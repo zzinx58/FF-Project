3cecd0b9e089a2e47f70b7f24d8c22ede3ed2db8
zzinx58 <q1095568627@163.com>
2023-04-27 03:08:48 +0800

    feat: backend schema and some chore

--- apps/main1_backend/src/admin/app.js
+++ apps/main1_backend/src/admin/app.js
@@ -20,7 +20,7 @@ export default {
         "Auth.form.welcome.subtitle": "登陆您的 BTF 账户",
         "global.plugins.content-type-builder": "模型构建器",
         "app.components.HomePage.welcomeBlock.content":
-          "恭喜! 您已登录为第一个管理员。为了探索 BTF-CMS 提供的强大功能，请创建一个你的内容类型",
+          "恭喜! 您已登录为管理员。为了探索 BTF-CMS 提供的强大功能，请创建一个你的内容类型",
         "app.components.HomePage.create": "创建一个你的内容类型",
       },
     },
